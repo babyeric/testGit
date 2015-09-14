f75ec7286a7494240cb4ec8e870ef916c04c5b16
babyeric <babyeric1982@gmail.com>
2015-09-14 14:55:58 -0700

    flyway support configuration
    flyway support configuration

--- flyway/src/main/java/com/practice/FlywayApplication.java
+++ flyway/src/main/java/com/practice/FlywayApplication.java
@@ -1,9 +1,12 @@
 package com.practice;
 
+import com.practice.config.EnvironmentAwarePropertyConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.CollectionUtils;
 
 /**
  * Created by Eric on 9/10/2015.
@@ -18,6 +21,13 @@ public class FlywayApplication {
 
         MigrationManager migrationManager = (MigrationManager)ctx.getBean("migrationManager");
         migrationManager.migrate();
+    }
+
+    @Bean
+    static EnvironmentAwarePropertyConfigurer propertyPlaceHolderConfigurer() {
+        EnvironmentAwarePropertyConfigurer pagePropertySourcePlaceHolderConfigurer = new EnvironmentAwarePropertyConfigurer();
+        pagePropertySourcePlaceHolderConfigurer.setPropertyBags(CollectionUtils.arrayToList(new String[]{"springDB"}));
+        return pagePropertySourcePlaceHolderConfigurer;
     }
 
 }
