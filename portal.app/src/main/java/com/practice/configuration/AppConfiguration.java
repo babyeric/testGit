2600268ab707e0e4592f0e9a8cf40d069154f9ee
babyeric <babyeric1982@gmail.com>
2015-09-25 17:10:05 -0700

    override velocity configuration
    override velocity configuration

--- portal.app/src/main/java/com/practice/configuration/AppConfiguration.java
+++ portal.app/src/main/java/com/practice/configuration/AppConfiguration.java
@@ -1,13 +1,19 @@
 package com.practice.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
+import org.springframework.boot.autoconfigure.velocity.VelocityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.ui.velocity.VelocityEngineFactory;
 import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.Properties;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,18 +23,25 @@ import javax.annotation.Resource;
  * To change this template use File | Settings | File Templates.
  */
 @Configuration
-@AutoConfigureAfter(VelocityAutoConfiguration.class)
-@Import(VelocityAutoConfiguration.class)
+@EnableConfigurationProperties(VelocityProperties.class)
 public class AppConfiguration {
-    @Resource
-    private VelocityConfigurer velocityConfigurer;
 
-    @PostConstruct
-    public void configVelocity() {
-        velocityConfigurer.getVelocityEngine().setProperty("spring.resource.loader.cache", "false");
-        int i=0;
-        ++i;
-        return;
+    @Autowired
+    private VelocityProperties properties;
+
+    @Bean
+    public VelocityConfigurer velocityConfigurer() {
+        VelocityConfigurer configurer = new VelocityConfigurer();
+        applyProperties(configurer);
+        return configurer;
+    }
+
+    private void applyProperties(VelocityEngineFactory factory) {
+        factory.setResourceLoaderPath(this.properties.getResourceLoaderPath());
+        factory.setPreferFileSystemAccess(this.properties.isPreferFileSystemAccess());
+        Properties velocityProperties = new Properties();
+        velocityProperties.putAll(this.properties.getProperties());
+        factory.setVelocityProperties(velocityProperties);
     }
 
 }
