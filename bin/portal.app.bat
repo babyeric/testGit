7944196a7223d3309c098365dfaab7e4a7d2620a
babyeric <babyeric1982@gmail.com>
2015-09-26 15:25:59 -0700

    windows command line
    windows command line

---
+++ bin/portal.app.bat
@@ -0,0 +1,7 @@
+SET DEBUG_OPT=-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n
+SET JAVA_OPT=%DEBUG_OPT% 
+SET JAR_PATH=.\portal.app\target\portal.app-0.1-SNAPSHOT.jar
+
+pushd ..
+start java %JAVA_OPT% -jar %JAR_PATH%
+popd
