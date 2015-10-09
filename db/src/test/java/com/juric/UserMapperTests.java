f7e6d2ae5ef2db38888d03154e30dc3fbb5d4180
babyeric <babyeric1982@gmail.com>
2015-10-08 23:35:18 -0700

    add ut, not finished
    add ut, not finished

---
+++ db/src/test/java/com/juric/UserMapperTests.java
@@ -0,0 +1,44 @@
+package com.juric;
+
+import com.practice.config.EnvironmentAwarePropertyConfigurer;
+import com.practice.configuration.DBConfiguration;
+import com.practice.user.UserDB;
+import com.practice.user.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.util.CollectionUtils;
+
+/**
+ * Created by Eric on 10/8/2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {UserMapperTests.class, DBConfiguration.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class})
+public class UserMapperTests {
+
+        @Bean
+        static EnvironmentAwarePropertyConfigurer propertyPlaceHolderConfigurer() {
+                EnvironmentAwarePropertyConfigurer pagePropertySourcePlaceHolderConfigurer = new EnvironmentAwarePropertyConfigurer();
+                pagePropertySourcePlaceHolderConfigurer.setPropertyBags(CollectionUtils.arrayToList(new String[]{"db"}));
+                return pagePropertySourcePlaceHolderConfigurer;
+        }
+
+        @Autowired
+        UserMapper userMapper;
+
+        @Test
+        public void testInsertUser() {
+                UserDB userDB = new UserDB();
+                userMapper.insert(userDB);
+        }
+}
