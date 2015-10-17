a10fc91b591460321c09bbbc03b81f83b6461825
babyeric <babyeric1982@gmail.com>
2015-10-17 14:07:34 -0700

    user controller test
    user controller test

--- carbon/src/test/java/com/juric/carbon/controller/UserControllerTests.java
+++ carbon/src/test/java/com/juric/carbon/controller/UserControllerTests.java
@@ -5,6 +5,7 @@ import com.juric.carbon.configuration.CarbonConfiguration;
 import com.juric.carbon.controller.AbstractControllerTest;
 import com.juric.carbon.schema.user.User;
 import com.juric.carbon.schema.user.UserCreate;
+import com.practice.utils.DateUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Date;
+import java.util.Random;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -34,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTests extends AbstractControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
-
+    private ObjectMapper objectMapper = new ObjectMapper();
     private MockMvc mockMvc;
 
     @Before
@@ -58,15 +60,41 @@ public class UserControllerTests extends AbstractControllerTest {
         update.setEmail("update@email.com");
         update.setModifiedBy("testUpdateUser");
 
-        ObjectMapper objectMapper = new ObjectMapper();
         String content = objectMapper.writeValueAsString(update);
 
-        this.mockMvc.perform(put("/1/users")
+        mockMvc.perform(put("/1/users")
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testGetUserById() throws Exception {
+        User user = createUser();
+        Assert.assertNotNull(user.getUserId());
+
+        String url = "/1/users/" + user.getUserId();
+        MvcResult mvcResult = mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+                .andReturn();
+
+        User result = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), User.class);
+        Assert.assertEquals(user.toString(), result.toString());
+    }
+
+    @Test
+    public void testGetUserByEmail() throws Exception {
+        User user = createUser();
+        Assert.assertNotNull(user.getUserId());
+
+        String url = "/1/users/?email=" + user.getEmail();
+        MvcResult mvcResult = mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
 
+        User result = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), User.class);
+        Assert.assertEquals(user.toString(), result.toString());
     }
 
     private User createUser() throws Exception{
@@ -75,14 +103,13 @@ public class UserControllerTests extends AbstractControllerTest {
         user.setFirstName("first name");
         user.setMobile("11223344");
         user.setCountry("CN");
-        user.setBirthday(new Date());
-        user.setEmail("test@emial.com");
+        user.setBirthday(DateUtils.parseDate("2001-12-13"));
+        user.setEmail("test" + new Random().nextLong()+"@email.com");
         user.setModifiedBy("UT");
 
         UserCreate userCreate = new UserCreate();
         userCreate.setUser(user);
         userCreate.setUserPassword("test password");
-        ObjectMapper objectMapper = new ObjectMapper();
         String content = objectMapper.writeValueAsString(userCreate);
         MvcResult mvcResult = this.mockMvc.perform(post("/1/users")
                 .content(content)
