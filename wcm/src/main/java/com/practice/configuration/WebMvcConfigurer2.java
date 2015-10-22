2e724a4372840b4b8f5643ad27e1850ec6b4f659
babyeric <babyeric1982@gmail.com>
2015-10-21 22:05:05 -0700

    config max upload
    config max upload

--- wcm/src/main/java/com/practice/configuration/WebMvcConfigurer2.java
+++ wcm/src/main/java/com/practice/configuration/WebMvcConfigurer2.java
@@ -1,5 +1,12 @@
 package com.practice.configuration;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,9 +16,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebMvcConfigurer2 extends WebMvcConfigurerAdapter {
+    @Value("${http.server.connector.maxPostSize}")
+    private int maxPostSize;
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new CSRFAddingHandlerInterceptor()).addPathPatterns("/**");
     }
+
+    @Bean
+    EmbeddedServletContainerCustomizer containerCustomizer() throws Exception {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+                    tomcat.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+                        @Override
+                        public void customize(Connector connector) {
+                            connector.setMaxPostSize(maxPostSize);
+                        }
+                    });
+                }
+            }
+        };
+    }
+
+
 }
